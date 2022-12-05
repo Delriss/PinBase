e3a8e088dcfaa000ce9a86290bf291f6ac166679
Delriss <haxz3rz@gmail.com>
2022-12-05 23:19:02 +0000

    Home Page + Input Form

---
+++ createPin.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Finley Pettit">
+    <link rel="icon" href="./img/favicon.ico" type="favicon">
+    <title>PinBase - New Pin</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Stylesheets -->
+
+</head>
+
+<body class="bg-white">
+
+    <!-- Navbar -->
+    <div class="container-fluid d-flex h-100 p-3 mx-auto flex-column text-light bg-dark">
+        <header class="mb-auto">
+            <div class="w-100 d-flex  justify-content-around">
+                <div class="d-flex justify content-center align-items-center">
+                    <img src="./img/logo.png" width="50" height="50" class="d-inline-block me-2">
+                    <h3 class="float-md-end mb-0"><a href="./index.php" class="text-white text-decoration-none">PinBase</a></h3>
+                </div>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                    <a class="nav-link active" href="./createPin.php">Create a Pin</a>
+                    <a class="nav-link" href="#">Login/Register</a>
+                </nav>
+            </div>
+        </header>
+    </div>
+    <!-- Navbar -->
+
+    <!-- Main -->
+
+    <!-- HEADER BLOCK -->
+    <div class="px-4 py-5 my-5 text-center">
+        <!-- Title -->
+        <h1 class="display-5 fw-bold">Create a Pin!</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Enter text to save and share. This can be: code, lyrics, documentation and more!</p>
+            <!-- Title -->
+            <!--Pin Name-->
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Pin Name</span>
+                <input type="text" class="form-control" placeholder="Pin Name" aria-label="Pin Name" aria-describedby="basic-addon1">
+            </div>
+            <!--Pin Name-->
+            <!--Pin Selectors-->
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="inputGroupSelect01">Visibility</label>
+                <select class="form-select" id="inputGroupSelect01">
+                    <option value="1">Public</option>
+                    <option value="2">Private</option>
+                </select>
+            </div>
+            <div class="input-group">
+                <label class="input-group-text">Expiry</label>
+                <div class="input-group-text">
+                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
+                </div>
+                <input type="text" class="form-control" aria-label="Text input with radio button" placeholder="DD/MM/YYYY">
+            </div>
+            <div class="input-group pt-3">
+                <label class="input-group-text">Password</label>
+                <div class="input-group-text">
+                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
+                </div>
+                <input type="text" class="form-control" aria-label="Text input with radio button" placeholder="Enter password...">
+            </div>
+            <!--Pin Selectors-->
+            <!--Pin Content-->
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center pt-3">
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Save Pin</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Clear Pin</button>
+            </div>
+            <!--Pin Content-->
+        </div>
+    </div>
+    <!-- HEADER BLOCK -->
+
+    <div class="b-example-divider"></div>
+
+    <!-- INPUT BLOCK -->
+    <div class="px-4 py-5 my-5 text-center">
+        <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="min-height: 175px"></textarea>
+            <label for="floatingTextarea2">Snippet Area</label>
+        </div>
+    </div>
+    <!-- INPUT BLOCK -->
+    <!-- Main -->
+
+    <!-- Footer -->
+    <footer class="container-fluid mt-auto text-white-50 bg-dark d-flex justify-content-center flex-column align-items-center">
+        <p><a href="./index.php" class="text-white">Pinbase</a> - Developed by Finley Pettit.</p>
+        <p>&copy; Del</p>
+    </footer>
+    <!-- Footer -->
+
+    <!-- Script Imports -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <!-- Script Imports -->
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Finley Pettit">
+    <link rel="icon" href="./img/favicon.ico" type="favicon">
+    <title>PinBase - Home</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap" rel="stylesheet">
+    <!-- Stylesheets -->
+
+</head>
+
+<body class="d-flex h-100 text-center text-white bg-dark">
+
+    <div class="area">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <!-- Navbar -->
+            <header class="mb-auto">
+                <div>
+                    <h3 class="float-md-start mb-0"><a href="./index.php" class="text-white text-decoration-none titleFont">PinBase</a></h3>
+                    <nav class="nav nav-masthead justify-content-center float-md-end">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                        <a class="nav-link" href="./createPin.php">Create a Pin</a>
+                        <a class="nav-link" href="#">Login/Register</a>
+                    </nav>
+                </div>
+            </header>
+            <!-- Navbar -->
+            <!--Main-->
+            <main class="px-3">
+                <img src="./img/logo.png" alt="PinBase" width="200" height="200" class="d-inline-block align-text-top">
+                <h1 class="display-1 titleFont">Welcome to PinBase!</h1>
+                <hr>
+                <p class="lead">Create saveable text snippets to store and share around with friends! PinBase is your own repository of information, with detailed statistics for each of your pins.</p>
+                <p class="lead">
+                    <a href="./createPin.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white text-dark">Create a Pin</a>
+                    <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white text-dark">View Pins</a>
+                </p>
+            </main>
+            <!--Main-->
+            <!--Footer-->
+            <footer class="mt-auto text-white-50">
+                <p><a href="./index.php" class="text-white">Pinbase</a> - Developed by Finley Pettit.</p>
+                <p>&copy; Del</p>
+            </footer>
+            <!--Footer-->
+        </div>
+
+
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+
+
+    <!-- Script Imports -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <!-- Script Imports -->
+</body>
+
+</html>
